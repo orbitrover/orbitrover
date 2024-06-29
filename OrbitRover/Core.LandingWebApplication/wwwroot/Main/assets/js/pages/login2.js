3c5e88dc028f82c51fffd9687121dfbe3b177bd6
himanilittle <mrs.himani@outlook.com>
2024-06-29 15:37:47 +0530

    adding wwwroot

---
+++ OrbitRover/Core.LandingWebApplication/wwwroot/Main/assets/js/pages/login2.js
@@ -0,0 +1,11 @@
+
+    $(document).ready(function() {
+        $(document).mousemove(function(event) {
+            TweenLite.to($('body'), .5, {css:{'background-position':parseInt(event.pageX/8) + "px "+parseInt(event.pageY/12)+"px, "+parseInt(event.pageX/15)+"px "+parseInt(event.pageY/15)+"px, "+parseInt(event.pageX/30)+"px "+parseInt(event.pageY/30)+"px"}});
+        });
+
+        //Flat red color scheme for iCheck
+        $('input[type="checkbox"].minimal-blue').iCheck({
+            checkboxClass: 'icheckbox_minimal-blue'
+        });
+    });
